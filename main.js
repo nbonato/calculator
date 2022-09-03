502e4ff1f5d3c66f4c953e5b7949f5313019f7e0
nbonato <akatux@gmail.com>
2022-09-03 23:30:38 +0200

    Add operate function and operators' functions
    The **operate** function calls one of the **operators** functions on the two numbers input based on the operator button pressed

--- main.js
+++ main.js
@@ -6,14 +6,69 @@ buttons.forEach(button => {
 
 const operators = ["+", "-", "x", "÷", "%"];
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let bot = "";
+
+function add(a, b) {
+    return a + b;
+};
+
+function multiplicate(a, b) {
+    return a * b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "÷":
+            divide(a, b);
+            break;
+    
+    }
+}
+
 
 function parseButton(e) {
     let pressedButton = e.target.textContent;
+    
     if (numbers.includes(pressedButton)) {
-        screen.textContent += e.target.textContent;
-    } else if (pressedButton === "AC") {
-        clearScreen();
-    }
+        if (!(pressedButton === "0" && screen.textContent === "0")) {
+            bot += e.target.textContent;
+        };
+    } else {
+        switch (pressedButton) {
+            case "AC":
+                clearScreen();
+                break;
+            case "C":
+                bot = bot.slice(0, -1);
+                break;
+            case "=":
+                console.log(parseFloat(screen.textContent));
+                break;
+            case ".":
+                if (!bot.includes(".")) {
+                    bot += ".";
+                }
+                break;
+        };
+    };
+    screen.textContent = bot;
 };
 
 function clearScreen() {
